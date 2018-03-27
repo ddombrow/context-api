c547924532373bf5bc30a707a7dbd96f79e62993
Dan Dombrowsky <ddombrow@gmail.com>
2018-03-26 23:58:07 -0400

    adding back the dropdown.

--- src/components/example0/index.js
+++ src/components/example0/index.js
@@ -2,22 +2,59 @@ import React from "react";
 import "react-dropdown/style.css";
 import PuppyStats from "./shared/PuppyStats";
 import PropTypes from "prop-types";
+import Dropdown from "react-dropdown";
 
 class Example0 extends React.Component {
+  state = {
+    locale: {
+      distance: "metric",
+      weight: "metric"
+    }
+  };
+
   getChildContext() {
-    return {
+    /*return {
       locale: {
         distance: "metric",
         weight: "imperial"
       }
-    };
+    };*/
+    return this.state;
   }
+
+  _onSelectWeight = e => {
+    const newState = Object.assign({}, this.state);
+    newState.locale.weight = e.value;
+    this.setState(newState);
+  };
+  _onSelectDistance = e => {
+    const newState = Object.assign({}, this.state);
+    newState.locale.distance = e.value;
+    this.setState(newState);
+  };
 
   render() {
     return (
       <div>
         <h2>Old Pups</h2>
         <PuppyStats />
+        <br />
+        Weight:{" "}
+        <Dropdown
+          options={["metric", "imperial"]}
+          onChange={this._onSelectWeight}
+          value={this.state.locale.weight}
+          defaultOption={this.state.locale.weight}
+          placeholder="Select an option"
+        />
+        <br />
+        Distance:{" "}
+        <Dropdown
+          options={["metric", "imperial"]}
+          onChange={this._onSelectDistance}
+          value={this.state.locale.distance}
+          placeholder="Select an option"
+        />
       </div>
     );
   }
